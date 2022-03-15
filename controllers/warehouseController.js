39417551263d7e383e29861bfdbfd023ff343fb2
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-03-15 11:17:50 +0000

    Removed class to simplify Model

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,6 +1,6 @@
-const Warehouse = require("../models/Warehouse");
+const warehouse = require("../models/Warehouse");
 
-const listAllWarehouses = (_req, res) => res.json(Warehouse.getAllWarehouses());
+const listAllWarehouses = (_req, res) => res.json(warehouse.getAllWarehouses());
 
 module.exports = {
 	listAllWarehouses,
--- models/Warehouse.js
+++ models/Warehouse.js
@@ -2,17 +2,8 @@ const path = require("path");
 const warehousesJSONPath = path.join(__dirname, "../data/warehouses.json");
 let warehouses = require(warehousesJSONPath);
 
-class Warehouse {
-	constructor(id, name, address, city, country, contact) {
-		this.id = id;
-		this.name = name;
-		this.address = address;
-		this.city = city;
-		this.country = country;
-		this.contact = contact;
-	}
+getAllWarehouses = () => warehouses;
 
-	static getAllWarehouses = () => warehouses;
-}
-
-module.exports = Warehouse;
+module.exports = {
+	getAllWarehouses,
+};
