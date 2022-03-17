443a732b38ada50758fa9f4c20a12717dd85d0bf
samirdewan1995 <samirdewan1995@gmail.com>
2022-03-17 13:28:02 +0000

    added catch and status to controller

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,6 +1,13 @@
 const inventory = require("../models/Inventory");
 
-const listAllInventories = (_req, res) => res.json(inventory.getAllInventories());
+const listAllInventories = (_req, res) => {
+    try {
+        res.status(200).json(inventory.getAllInventories());
+        }
+    catch {
+        res.status(400).json({"errorMessage": "the list of inventories could not be retrieved."});
+    }
+}
 
 module.exports = {
   listAllInventories,
