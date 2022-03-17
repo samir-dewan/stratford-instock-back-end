5f6b68657c61924478c4ea8233d030ec21652dea
Wahaj Arsal <a.wahaj@hotmail.co.uk>
2022-03-17 20:42:00 +0000

    saving before dev pull

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 /** @format */
 
 require("dotenv").config();
-
+const { randomUUID } = require("crypto");
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-let warehouseDataAll = JSON.parse(fs.readFileSync("./data/warehouse.json"));
-let inventoriesData = JSON.parse(fs.readFileSync("./data/inventories.json"));
+// let warehouseDataAll = JSON.parse(fs.readFileSync("./data/warehouse.json"));
+// let inventoriesData = JSON.parse(fs.readFileSync("./data/inventories.json"));
 
 app.listen(PORT);
+app.use(cors());
+app.use(express.json());
+
+app.get("/randomId", (req, res) => {
+  const newId = randomUUID();
+  res.send(newId);
+});
 
 //******** API To POST/CREATE A Warehouse ******** */
 app.post("/warehouse/add-new", (req, res) => {
@@ -21,11 +29,11 @@ app.post("/warehouse/add-new", (req, res) => {
     city: req.body.city,
     country: req.body.country,
     contact: {
-    contactName: req.body.contactName,
-    position: req.body.position,
-    phoneNumber: req.body.phoneNumber,
-    email: req.body.email,
-    }
+      contactName: req.body.contactName,
+      position: req.body.position,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+    },
   };
 
   const warehouseContent = JSON.parse(fs.readFileSync("./data/warehouse.json"));
