44f786e77619d2492abf9bbe9d093c26bf14f84e
102road <bill.brown094@gmail.com>
2022-03-19 20:10:42 +0000

    add functionality for getting inventory by warehouseId via the route /inventories/warehouse/:warehouseId

--- models/Inventory.js
+++ models/Inventory.js
@@ -4,15 +4,20 @@ const fs = require("fs");
 const inventoriesJSONPath = path.join(__dirname, "../data/inventories.json");
 let inventories = require(inventoriesJSONPath);
 
-
 getAllInventories = () => inventories;
 
 getSingleInventory = (id) => {
-    const currInventory = inventories.find((inventory) => inventory.id === id);
-        return currInventory;
-}
+  const currInventory = inventories.find((inventory) => inventory.id === id);
+  return currInventory;
+};
+
+getInventoryByWarehouseId = (id) => {
+  let arr = inventories.filter((item) => item.warehouseID === id);
+  return arr;
+};
 
 module.exports = {
   getAllInventories,
-  getSingleInventory
+  getSingleInventory,
+  getInventoryByWarehouseId
 };
