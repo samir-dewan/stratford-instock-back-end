e49c830cd13c2db8bef6a03c34ac3bca6aa1a87d
102road <bill.brown094@gmail.com>
2022-03-19 20:36:56 +0000

    add functionality for getting a warehouse and its inventory via /warehouses/:warehouseId

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -4,11 +4,9 @@ const listAllInventories = (_req, res) => {
   try {
     res.status(200).json(inventory.getAllInventories());
   } catch {
-    res
-      .status(400)
-      .json({
-        errorMessage: "the list of inventories could not be retrieved.",
-      });
+    res.status(400).json({
+      errorMessage: "the list of inventories could not be retrieved.",
+    });
   }
 };
 
