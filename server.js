c3c3f35287086d59b948da90c061d4552c9a779e
Wahaj-Arsal <a.wahaj@hotmail.co.uk>
2022-03-21 17:18:54 +0000

    added single get for warehouse

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const warehouseRouter = require("./routes/warehouses");
 const inventoryRouter = require("./routes/inventories");
 
 app.listen(PORT, () => {
-	console.log(`App is listening on port ${PORT}`);
+  console.log(`App is listening on port ${PORT}`);
 });
 
 app.use(cors());
@@ -25,80 +25,80 @@ app.use("/warehouses", warehouseRouter);
 app.use("/inventories", inventoryRouter);
 
 app.get("/randomId", (req, res) => {
-	const newId = uniqid();
-	res.send(newId);
+  const newId = uniqid();
+  res.send(newId);
 });
 
 //******** API To POST/CREATE A Warehouse ******** */
 app.post("/warehouse/add-new", (req, res) => {
-	const newWarehouse = {
-		id: uniqid(),
-		warehouseName: req.body.warehouseName,
-		streetAddress: req.body.streetAddress,
-		city: req.body.city,
-		country: req.body.country,
-		contact: {
-			contactName: req.body.contact.contactName,
-			position: req.body.contact.position,
-			phoneNumber: req.body.contact.phoneNumber,
-			email: req.body.contact.email,
-		},
-	};
-	const warehouseContent = JSON.parse(
-		fs.readFileSync("./data/warehouses.json")
-	);
-	warehouseContent.push(newWarehouse);
-	fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseContent));
-	res.status(201).json(newWarehouse);
+  const newWarehouse = {
+    id: uniqid(),
+    warehouseName: req.body.warehouseName,
+    streetAddress: req.body.streetAddress,
+    city: req.body.city,
+    country: req.body.country,
+    contact: {
+      contactName: req.body.contact.contactName,
+      position: req.body.contact.position,
+      phoneNumber: req.body.contact.phoneNumber,
+      email: req.body.contact.email,
+    },
+  };
+  const warehouseContent = JSON.parse(
+    fs.readFileSync("./data/warehouses.json")
+  );
+  warehouseContent.push(newWarehouse);
+  fs.writeFileSync("./data/warehouses.json", JSON.stringify(warehouseContent));
+  res.status(201).json(newWarehouse);
 });
 
 //******** API To POST/CREATE A Warehouse ******** */
-// app.get("/:warehouseId", (req, res) => {
-//   const warehouseId = req.params.warehouseId;
-//   const fileContent = JSON.parse(fs.readFileSync("./data/warehouses.json"));
-//   for (let i = 0; i < fileContent.length; i++) {
-//     if (fileContent[i].id == warehouseId) {
-//       res.status(200).send(fileContent[i]);
-//     }
-//   }
-// });
+app.get("/:warehouseId", (req, res) => {
+  const warehouseId = req.params.warehouseId;
+  const fileContent = JSON.parse(fs.readFileSync("./data/warehouses.json"));
+  for (let i = 0; i < fileContent.length; i++) {
+    if (fileContent[i].id == warehouseId) {
+      res.status(200).send(fileContent[i]);
+    }
+  }
+});
 
 app.delete("/:warehouseId", (req, res) => {
-	const warehouseId = req.params.warehouseId;
-	const warehouseContent = JSON.parse(
-		fs.readFileSync("./data/warehouses.json")
-	);
-	const inventoryItems = JSON.parse(fs.readFileSync("./data/inventories.json"));
-	const filteredWarehouses = warehouseContent.filter(
-		(warehouses) => warehouses.id !== warehouseId
-	);
-	// console.log(filteredWarehouses);
-	const filteredInventory = inventoryItems.filter(
-		(inventory) => inventory.warehouseID !== warehouseId
-	);
-	// console.log(filteredInventory);
+  const warehouseId = req.params.warehouseId;
+  const warehouseContent = JSON.parse(
+    fs.readFileSync("./data/warehouses.json")
+  );
+  const inventoryItems = JSON.parse(fs.readFileSync("./data/inventories.json"));
+  const filteredWarehouses = warehouseContent.filter(
+    (warehouses) => warehouses.id !== warehouseId
+  );
+  // console.log(filteredWarehouses);
+  const filteredInventory = inventoryItems.filter(
+    (inventory) => inventory.warehouseID !== warehouseId
+  );
+  // console.log(filteredInventory);
 
-	fs.writeFileSync(
-		"./data/warehouses.json",
-		JSON.stringify(filteredWarehouses),
-		"utf8",
-		(err) => {
-			if (err) {
-				console.log(err);
-			}
-		}
-	);
-	res.status(200).json(filteredWarehouses);
+  fs.writeFileSync(
+    "./data/warehouses.json",
+    JSON.stringify(filteredWarehouses),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  res.status(200).json(filteredWarehouses);
 
-	fs.writeFileSync(
-		"./data/inventories.json",
-		JSON.stringify(filteredInventory),
-		"utf8",
-		(err) => {
-			if (err) {
-				console.log(err);
-			}
-		}
-	);
-	res.status(200).json(filteredInventory);
+  fs.writeFileSync(
+    "./data/inventories.json",
+    JSON.stringify(filteredInventory),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  res.status(200).json(filteredInventory);
 });
